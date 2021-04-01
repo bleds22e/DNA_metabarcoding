6f27eada9e7d743ff1aa31caaaa7872fbd08b56e
Glenda Yenni <glenda@weecology.org>
2021-04-01 17:35:57 -0600

    Add genus-level plots to practice_script

--- scripts/practice_script.R
+++ scripts/practice_script.R
@@ -23,11 +23,17 @@ krat_sp_its <- left_join(krat_its,
 krat_sp_its <- krat_sp_its %>% 
   rename("rodent_sp" = "species") %>% 
   filter(Reads >= 5,
-         Domain == "Eukaryota")
+         Domain == "Eukaryota", 
+         !is.na(Genus))
+
+all_samples <- krat_sp_its %>%        
+  expand(SampleID, Genus)
 
 # find the average number of reads per plant family for each krat species
-krat_family_reads <- krat_sp_its %>%          # use the krat_sp_its df
-  group_by(rodent_sp, Family, period) %>%     # group the data by these 3 columns
+krat_family_reads <- krat_sp_its %>% 
+  full_join(all_samples) %>%                  # use the krat_sp_its df 
+  group_by(rodent_sp, Family, period) %>% 
+  replace_na(list(0)) %>%                                  # group the data by these 3 columns
   summarize(avg_family_reads = mean(Reads))   # make a new column called avg_family_reads
                                               # by taking the mean of the Reads column for 
                                               # for each group that we made above 
@@ -44,21 +50,118 @@ ggplot(data = krat_family_reads, aes(x = Family, y = avg_family_reads)) +
 
 
 ### What about with Genus?
-# find the average number of reads per plant family for each krat species
-krat_Genus_reads <- krat_sp_its %>%          # use the krat_sp_its df
-  group_by(rodent_sp, Genus, period) %>%     # group the data by these 3 columns
-  summarize(avg_genus_reads = mean(Reads))   # make a new column called avg_family_reads
-# use the package ggplot2 to make some plots
-ggplot(data = krat_Genus_reads, aes(x = Genus, y = avg_genus_reads)) +
+# find the average number of reads per plant genus for each krat species
+krat_genus_reads <- krat_sp_its %>% 
+  full_join(all_samples) %>%                  
+  group_by(rodent_sp, Genus, period) %>% 
+  replace_na(list(0)) %>%  
+  mutate(Genus=replace(Genus,Genus=="Hoffmannseggia","Hoffmanseggia")) %>%
+  summarize(avg_genus_reads = mean(Reads)) %>%
+  drop_na() %>%
+  mutate(type = "Rodents")
+
+# create color palate for ggplot to use
+pal <- c(
+  "DM" = "salmon",
+  "DO" = "#4E84C4", 
+  "abundance" = "#52854C"
+  )
+
+# Genus-level plots
+ggplot(data = krat_genus_reads, aes(x = Genus, y = avg_genus_reads, fill = rodent_sp)) +
   # tell ggplot what df to use and which columns will be the values on the x and y axes
-  geom_col() + # add a geom (like a shape) to plot--this one is saying make a plot with columns
-  facet_wrap(period ~ rodent_sp,     # split the plot into multiple plots, making a mini plot for 
-             nrow = 3, ncol = 2) +   # each combination of period and krat. Make it have 3 rows and 2 columns
+  geom_col(width = .5, position = position_dodge(1)) + # add a geom (like a shape) to plot--this one is saying make a plot with columns
+  facet_wrap(~period, nrow=3) +   
   theme_bw() + # make it prettier
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 8))
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 8)) +
+  scale_fill_manual(name = "" , values = pal, limits = names(pal))
+
+# Bring in portal plant and rodent data
+tables <- portalr::load_plant_data()
+rodents <- portalr::load_rodent_data()
+
+plants <- portalr::plant_abundance(level="treatment") %>%
+  filter(treatment=="control") %>%
+  left_join(tables$species_table) %>%
+  filter(year %in% c(2016, 2017)) %>%
+  filter(!(year == 2016 & season == "winter")) %>%
+  mutate(period = 454, Genus=genus, type="Plants") %>%
+  mutate(period = ifelse(year==2017 & season=="winter", 460, period)) %>%
+  mutate(period = ifelse(year==2017 & season=="summer", 466, period)) %>%
+  mutate(rodent_sp = "abundance") %>%
+  select(rodent_sp, Genus, period, abundance, type) %>%
+  group_by(rodent_sp, period, Genus, type) %>%
+  summarise(avg_genus_reads = sum(abundance))
+
+# Combine dna and plant tables, add season variable for plotting
+
+rodents_plants <- bind_rows(krat_genus_reads, plants) %>%
+  mutate(season = "Summer 2016") %>%
+  mutate(season = replace(season, period==460, "Winter 2017"),
+         season = replace(season, period==466, "Summer 2017"))
+
+# Plot all data
+
+ggplot(data = rodents_plants, aes(x = Genus, y = avg_genus_reads, fill = rodent_sp)) +
+  geom_col(width = .5, position = position_dodge(1)) +
+  facet_wrap(~season, nrow=3) +   
+  scale_y_log10(name = "count") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 8)) +
+  scale_fill_manual(name = "" , values = pal, limits = names(pal))
+
+
+# Focus on some highlights
+# Asynchrony
+rodents_plants %>%
+  filter(Genus %in% c("Astragalus", "Amaranthus", "Bouteloua")) %>%
+ggplot(aes(x = Genus, y = avg_genus_reads, fill = rodent_sp)) +
+  geom_col(position = "dodge") +
+  facet_wrap(~season, nrow=3) +   
+  scale_y_log10(name = "count") +
+  theme_bw() + # make it prettier
+  theme(axis.text.x = element_text(size = 10)) +
+  scale_fill_manual(name = "" , values = pal, limits = names(pal))
+
+# Legume family
+rodents_plants %>%
+  filter(Genus %in% c("Astragalus","Vachellia","Dalea","Hoffmanseggia","Prosopis","Lupinus")) %>%
+  ggplot(aes(x = Genus, y = avg_genus_reads, fill = rodent_sp)) +
+  geom_col(position = "dodge") + 
+  facet_wrap(~season, nrow=3) +   
+  scale_y_log10(name = "count") +
+  theme_bw() + # make it prettier
+  theme(axis.text.x = element_text(size = 9)) +
+  scale_fill_manual(name = "" , values = pal, limits = names(pal))
+
+# Grasses
+rodents_plants %>%
+  filter(Genus %in% c("Aristida","Bouteloua","Eragrostis","Muhlenbergia","Munroa","Setaria",     
+                      "Eriochloa","Panicum","Urochloa","Hordeum","Enneapogon","Digitaria","Hilaria",     
+                      "Yakirra","Coleataenia","Chondrosum","Bromus","Sporobolus","Tragus","Digitaria",
+                      "Erioneuron","Vulpia","Schismus","Lycurus","Chloris","Enneapogon","Buchloe")) %>%
+  ggplot(aes(x = Genus, y = avg_genus_reads, fill = rodent_sp)) +
+  geom_col(position = "dodge") + 
+  facet_wrap(~season, nrow=3) +   
+  scale_y_log10(name = "count") +
+  theme_bw() + # make it prettier
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.6, size = 9)) +
+  scale_fill_manual(name = "" , values = pal, limits = names(pal))
+
+# DO Favorites
+rodents_plants %>%
+  filter(Genus %in% c("Plantago","Xanthisma","Nuttallanthus","Linanthus", "Kallestromia", "Eschscholzia",
+                      "Euthamia")) %>%
+  ggplot(aes(x = Genus, y = avg_genus_reads, fill = rodent_sp)) +
+  geom_col(position = "dodge") + 
+  facet_wrap(~season, nrow=3) +   
+  scale_y_log10(name = "count") +
+  theme_bw() + # make it prettier
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.6, size = 9)) +
+  scale_fill_manual(name = "" , values = pal, limits = names(pal))
+  
 
 # Other things that you might want to investigate or think about:
-# - remove any OTUs that have fewer than 10 or 50 or 100 reads to focus on the majority of the diet
 # - use presence/absense and see which plants DMs and DOs have in common
 # - compare what the krats are eating to what plants we find on the control plots
 # - use a diet selectivity metric to see how what plants DMs and DOs select compare to 
